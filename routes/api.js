484ac8e7a38dae5a04646899301b5e4609396208
Dương Thành Dũng <14dh110224@st.huflit.edu.vn>
2022-10-19 19:39:24 +0700

    Adding mongodb connection.
    Changes to be committed:
    	modified:   .env.dist
    	modified:   app.js
    	modified:   bin/www
    	new file:   helpers/connector.js
    	new file:   models/subscription.js
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   routes/api.js
    	modified:   views/index.jade
    	modified:   views/layout.jade

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var Subscription = require('../models/subscription');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Mail Form API' });
+router.post('/', function(req, res, next) {
+    let data = req.body;
+    let subscription = new Subscription({
+        fullName: data.fullName,
+        email: data.email,
+        phone: data.phone,
+        message: data.message,
+    });
+
+    subscription.save(function (err, Subscription) {
+        if(err) {
+            res.render(
+                       'error',
+                        {
+                            message: "Something went wrong",
+                            error: err
+                        }
+            );
+        } else {
+            res.render('index', { title: 'Subscription saved' });
+        }
+    })
 });
 
 module.exports = router;
