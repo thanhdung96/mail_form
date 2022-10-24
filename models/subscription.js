227201814ef269ce220e6d4dc94f762994d33f50
thanhdung96 <14dh110224@st.huflit.edu.vn>
2022-10-24 10:12:27 +0700

    Added timestamp to subscription.
    Changes to be committed:
    	modified:   helpers/mailer.js
    	modified:   models/subscription.js
    	modified:   routes/api.js
    	modified:   views/mail_subscription.jade

--- models/subscription.js
+++ models/subscription.js
@@ -1,13 +1,18 @@
 var mongoose = require('../helpers/connector');
 
-var subscriptionSchema = mongoose.Schema({
-   jobId: String,
-   fullName: String,
-   email: String,
-   phone: String,
-   message: String,
-   file: String
-});
+var subscriptionSchema = mongoose.Schema(
+   {
+      jobId: String,
+      fullName: String,
+      email: String,
+      phone: String,
+      message: String,
+      file: String
+   },
+   {
+      timestamps: true
+   }
+);
 
 var Subscription = mongoose.model("Subscription", subscriptionSchema);
 
